5c4f2c095dc8c02be0c79f3f22f6d4b6488db1df
Dax <daaawx@gmail.com>
2020-08-29 00:37:55 +0200

    Create proxy catchall for development

--- backend/todolist/settings.py
+++ backend/todolist/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/asdfasldfa/'
 STATICFILES_DIRS = [
     os.path.join(FRONTEND_DIR, 'build', 'static')
 ]
--- backend/todolist/views.py
+++ backend/todolist/views.py
@@ -1,3 +1,39 @@
+from django.conf import settings
+from django.http import HttpResponse, StreamingHttpResponse
+from django.template import engines
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
+import requests
 
-catchall = TemplateView.as_view(template_name='index.html')
+
+@csrf_exempt
+def catchall_dev(request, upstream='http://localhost:3000'):
+    upstream_url = upstream + request.path
+    method = request.META['REQUEST_METHOD'].lower()
+    r = getattr(requests, method)(upstream_url, stream=True)
+    content_type = r.headers.get('Content-Type')
+
+    if request.META.get('HTTP_UPGRADE', '').lower() == 'websocket':
+        return HttpResponse(
+            content='WebSocket connections not supported',
+            status=501,
+            reason='Not implemented',
+        )
+    elif content_type == 'text/html; charset=UTF-8':
+        return HttpResponse(
+            content=engines['django'].from_string(r.text).render(),
+            status=r.status_code,
+            reason=r.reason,
+        )
+    else:
+        return StreamingHttpResponse(
+            streaming_content=r.iter_content(4096),
+            content_type=content_type,
+            status=r.status_code,
+            reason=r.reason,
+        )
+
+
+catchall_prod = TemplateView.as_view(template_name='index.html')
+
+catchall = catchall_dev if settings.DEBUG else catchall_prod
