79119a2d141fb16b9ce065ab8c9c47836fda6639
Dax <daaawx@gmail.com>
2020-08-29 23:08:56 +0200

    Fix static url

--- backend/todolist/settings.py
+++ backend/todolist/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = '/asdfasldfa/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(FRONTEND_DIR, 'build', 'static')
 ]
